08ed7b67eacdadc056289ecfabe0da9fc87f36dd
luca.dibello@student.supsi.ch <info@lucadibello.ch>
2022-02-23 20:29:50 +0100

    Updated homepage screen

--- greenhouse-app/app/screens/homepage/homepage-screen.tsx
+++ greenhouse-app/app/screens/homepage/homepage-screen.tsx
@@ -1,21 +1,31 @@
 import React, { FC } from "react"
+import { StyleSheet } from "react-native"
 import { observer } from "mobx-react-lite"
-import { View } from "react-native"
 import { StackScreenProps } from "@react-navigation/stack"
 import { TabParamList } from "../../navigators"
-import { GradientBackground, Screen } from "../../components"
-import { t, color } from 'react-native-tailwindcss'
+import { Layout, Text } from "@ui-kitten/components"
 
 export const HomepageScreen: FC<StackScreenProps<TabParamList, "homepage">> = observer(
   ({navigation}) => {
     // Pull in one of our MST stores
     return (
-      <View testID="HomepageScreen" style={t.flex1}>
-        <GradientBackground colors={["#52b69a", "#76c893"]} />
-        <Screen style={t.mT10} backgroundColor={color.transparent}>
-          { /* Page content here */ }
-        </Screen>
-      </View>
+      <Layout style={styles.container}>
+        <Text category='h2'>My greenhouses</Text>
+        <Text>...List here...</Text>
+
+        <Text category='h2'>My greenhouses</Text>
+
+      </Layout>
     )
   }
 )
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingLeft: "3%"
+  },
+  title: {
+    textDecorationLine: "underline"
+  }
+})
