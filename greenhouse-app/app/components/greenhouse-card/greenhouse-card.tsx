0403c2b92fe4ca420341c593126944602a872f56
luca.dibello@student.supsi.ch <info@lucadibello.ch>
2022-02-23 21:18:28 +0100

    Generated new empty component for the greenhouse information

---
+++ greenhouse-app/app/components/greenhouse-card/greenhouse-card.tsx
@@ -0,0 +1,36 @@
+import * as React from "react"
+import { StyleProp, TextStyle, View, ViewStyle } from "react-native"
+import { observer } from "mobx-react-lite"
+import { color, typography } from "../../theme"
+import { Text } from "../text/text"
+
+const CONTAINER: ViewStyle = {
+  justifyContent: "center",
+}
+
+const TEXT: TextStyle = {
+  fontFamily: typography.primary,
+  fontSize: 14,
+  color: color.primary,
+}
+
+export interface GreenhouseCardProps {
+  /**
+   * An optional style override useful for padding & margin.
+   */
+  style?: StyleProp<ViewStyle>
+}
+
+/**
+ * Describe your component here
+ */
+export const GreenhouseCard = observer(function GreenhouseCard(props: GreenhouseCardProps) {
+  const { style } = props
+  const styles = Object.assign({}, CONTAINER, style)
+
+  return (
+    <View style={styles}>
+      <Text style={TEXT}>Hello</Text>
+    </View>
+  )
+})
