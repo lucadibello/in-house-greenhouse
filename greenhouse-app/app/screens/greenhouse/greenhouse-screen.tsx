5f994586f831fc736f4898ddbebed3c8a0e60c7d
luca.dibello@student.supsi.ch <info@lucadibello.ch>
2022-02-24 22:32:55 +0100

    Showing greenhouse information inside greenhouse screen

--- greenhouse-app/app/screens/greenhouse/greenhouse-screen.tsx
+++ greenhouse-app/app/screens/greenhouse/greenhouse-screen.tsx
@@ -1,27 +1,37 @@
 import React, { FC } from "react"
 import { observer } from "mobx-react-lite"
-import { ViewStyle } from "react-native"
+import { StyleSheet } from "react-native"
 import { StackScreenProps } from "@react-navigation/stack"
 import { NavigatorParamList } from "../../navigators"
-import { Screen, Text } from "../../components"
-// import { useNavigation } from "@react-navigation/native"
-// import { useStores } from "../../models"
-import { color } from "../../theme"
-
-const ROOT: ViewStyle = {
-  backgroundColor: color.palette.black,
-  flex: 1,
-}
+import { Layout, Text } from "@ui-kitten/components"
+import { RouteProp, useNavigation, useRoute } from "@react-navigation/native"
 
 export const GreenhouseScreen: FC<StackScreenProps<NavigatorParamList, "greenhouse">> = observer(function GreenhouseScreen() {
-  // Pull in one of our MST stores
-  // const { someStore, anotherStore } = useStores()
+  // Load navigation via hook
+  const navigation = useNavigation()
+  // Read route params
+  const route = useRoute<RouteProp<NavigatorParamList, 'greenhouse'>>();
+  
+  // Update page title
+  React.useEffect(() => {
+    navigation.setOptions({
+      title: route.params.details.title
+    })
+  }, [])
 
-  // Pull in navigation via hook
-  // const navigation = useNavigation()
+  // Show greenhouse inforamtion
   return (
-    <Screen style={ROOT} preset="scroll">
-      <Text preset="header" text="greenhouse" />
-    </Screen>
+    <Layout style={styles.container}>
+      <Text category='h2'>{route.params.details.title}, ID: {route.params.details.id}</Text>
+    </Layout>
   )
+})
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  notchBg: {
+    flex: 1
+  }
 })
