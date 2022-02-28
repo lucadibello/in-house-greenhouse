c43d27f5bbb8bf8901bbc36eae8f9afec0a64539
luca.dibello@student.supsi.ch <info@lucadibello.ch>
2022-02-28 23:11:13 +0100

    Created new graphql API

---
+++ greenhouse-server/src/api/graphql/User.ts
@@ -0,0 +1,43 @@
+import { extendType, nonNull, objectType, stringArg } from 'nexus'
+
+export const User = objectType({
+  name: 'User',
+  definition(t) {
+    t.nonNull.int('id')
+    t.nonNull.string('email')
+    t.nonNull.string('name')
+    t.nonNull.string('surname')
+    t.nonNull.string('password')
+  },
+})
+
+export const PostQuery = extendType({
+  type: 'Query',
+  definition(t) {
+    t.list.nonNull.field('users', {
+      type: 'User',
+      resolve(_, args, context) {
+        return context.prisma.user.findMany();
+      },
+    });
+    t.nonNull.field('addUser', {
+      type: 'User',
+      args: {
+        email: nonNull(stringArg()),
+        name: nonNull(stringArg()),
+        surname: nonNull(stringArg()),
+        password: nonNull(stringArg()),
+      },
+      resolve: async (_, args, context) => {
+        return context.prisma.user.create({
+          data: {
+            email: args.email,
+            name: args.name,
+            surname: args.surname,
+            password: args.password
+          }
+        })
+      }
+    })
+  },
+})
