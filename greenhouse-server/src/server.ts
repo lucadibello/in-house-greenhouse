da3ce8a379a915f9bd598ca76aef366dbe01ad56
luca.dibello@student.supsi.ch <info@lucadibello.ch>
2022-02-28 19:43:28 +0100

    Dotenv now loads .env files into environment variables

--- greenhouse-server/src/server.ts
+++ greenhouse-server/src/server.ts
@@ -2,11 +2,16 @@ import { ApolloServer } from 'apollo-server'
 import { schema } from './api/schema'
 import { context } from './api/context'
 
+// Configure dotenv
+require('dotenv').config()
+
+// Create Apollo Server
 const server = new ApolloServer({
   schema: schema,
   context: context,
 })
 
+// Start listening
 server.listen().then(async ({ url }) => {
   console.log(`\
     🚀 Server ready at: ${url}
