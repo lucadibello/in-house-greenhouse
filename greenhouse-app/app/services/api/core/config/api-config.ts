8098a123050a97f8a1d3b4fae6519bd781ad47b4
luca.dibello@student.supsi.ch <info@lucadibello.ch>
2022-03-02 22:15:30 +0100

    API now uses environment config

--- greenhouse-app/app/services/api/core/config/api-config.ts
+++ greenhouse-app/app/services/api/core/config/api-config.ts
@@ -1,7 +1,7 @@
 // Use this import if you want to use "env.js" file
 // const { API_URL } = require("../../config/env")
 // Or just specify it directly like this:
-const API_URL = "http://example.com"
+const { API_URL } = require("../../config/env")
 
 /**
  * The options used to configure the API.
