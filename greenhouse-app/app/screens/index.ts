23fc1b00023f057be32f5a16ce0f559f3484d4b7
luca.dibello@student.supsi.ch <info@lucadibello.ch>
2022-03-02 13:08:28 +0100

    Removed deleted screens from export

--- greenhouse-app/app/screens/index.ts
+++ greenhouse-app/app/screens/index.ts
@@ -1,6 +1,4 @@
 export * from "./welcome/welcome-screen"
-export * from "./demo/demo-screen"
-export * from "./demo/demo-list-screen"
 export * from "./error/error-boundary"
 // export other screens here
 export * from "./homepage/homepage-screen"
