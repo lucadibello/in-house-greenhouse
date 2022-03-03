9c543e08cc4422df552e3ec9c18c3799a8dd91b5
luca.dibello@student.supsi.ch <info@lucadibello.ch>
2022-03-03 14:57:06 +0100

    Edited plant model

--- greenhouse-app/app/models/plant/plant.ts
+++ greenhouse-app/app/models/plant/plant.ts
@@ -4,11 +4,11 @@ import { Instance, SnapshotOut, types } from "mobx-state-tree"
  * Greenhouse's plant model
  */
 export const PlantModel = types.model("Plant").props({
-  id: types.identifierNumber,
+  id: types.string,
   name: types.string,
   description: types.maybe(types.string),
-  created_at: types.Date,
-  updated_at: types.Date
+  created_at: types.string,
+  updated_at: types.string
 })
 
 type PlantType = Instance<typeof PlantModel>
