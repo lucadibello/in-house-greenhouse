e5743c9ab203ef98ff140be5c6b1cb01076444b4
luca.dibello@student.supsi.ch <info@lucadibello.ch>
2022-03-03 14:56:28 +0100

    Greenhouses store

--- greenhouse-app/app/models/greenhouse-store/greenhouse-store.ts
+++ greenhouse-app/app/models/greenhouse-store/greenhouse-store.ts
@@ -1,38 +1,40 @@
-import { applySnapshot, Instance, SnapshotOut, types } from "mobx-state-tree"
-import { withEnvironment } from "../extensions/with-environment";
-import { GreenhouseModel, GreenhouseSnapshot } from "../greenhouse/greenhouse";
+import { Instance, SnapshotOut, types, flow } from "mobx-state-tree"
 import { GreenhouseApi } from "../../services/api/greenhouse/greenhouse-api"
+import { withEnvironment } from "../extensions/with-environment"
+import { GreenhouseModel, GreenhouseSnapshot } from "../greenhouse/greenhouse"
 
 /**
- * Greenhouse store. Will contain all registered greenhouses
+ * Greenhouses store
  */
- export const GreenhouseStoreModel = types
- .model("GreenhouseStore")
- .props({
-   greenhouses: types.array(GreenhouseModel)
- })
- .extend(withEnvironment)
- .actions((self) => ({
-    saveGreenhouses: (greenhouseSnapshots: GreenhouseSnapshot[]) => {
-      console.tron.log("Replacing greenhouse snapshot with new ones...", greenhouseSnapshots)
-      // Apply snapshot
-      applySnapshot(self.greenhouses, greenhouseSnapshots)
+export const GreenhouseStoreModel = types
+  .model("GreenhouseStore")
+  .extend(withEnvironment)
+  .props({
+    greenhouses: types.array(GreenhouseModel),
+  })
+  .actions((self) => ({
+    empty () {
+      self.greenhouses.replace([])
     }
- }))
- .actions((self) => ({
-    getGreenhouses: async () => {
-      console.tron.log("Get greenhouses from greenhouse-store", self)
+  }))
+  .actions(self => ({
+    setGreenhouses (greenhouses: GreenhouseSnapshot[]) {
+      // Save new greenhouses
+      self.greenhouses.replace(greenhouses)
+    }
+  }))
+  .actions(self => ({
+    getGreenhouses: flow(function* getGreenhouses () {
       const greenhouseApi = new GreenhouseApi(self.environment.api)
-      const result = await greenhouseApi.getGreenhouses()
-      
+      const result = yield greenhouseApi.getGreenhouses()
+
       if (result.kind === "ok") {
-        console.tron.log("RECEIVED GREENHOUSES FROM APIs: ", result)
-        // FIXME: self.saveGreenhouses(result.data)
+        self.setGreenhouses(result.greenhouses)
       } else {
         __DEV__ && console.tron.log(result.kind)
       }
-    }
- }))
+    }) 
+  }))
 
 type GreenhouseStoreType = Instance<typeof GreenhouseStoreModel>
 export interface GreenhouseStore extends GreenhouseStoreType {}
