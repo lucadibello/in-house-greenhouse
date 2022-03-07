4ca7822c4981511acf243afd35b27eed45453e9f
luca.dibello@student.supsi.ch <info@lucadibello.ch>
2022-03-07 19:18:09 +0100

    Updated greenhouse card

--- greenhouse-app/app/components/greenhouse-card/greenhouse-card.tsx
+++ greenhouse-app/app/components/greenhouse-card/greenhouse-card.tsx
@@ -1,7 +1,7 @@
 import * as React from "react"
 import { StyleProp, StyleSheet, View, ViewStyle } from "react-native"
 import { observer } from "mobx-react-lite"
-import { Card, Layout, Text } from "@ui-kitten/components"
+import { Card, Layout, Text, Icon } from "@ui-kitten/components"
 import { Greenhouse } from "../../models/greenhouse/greenhouse";
 
 export interface GreenhouseCardProps {
@@ -16,29 +16,58 @@ interface HeaderProps {
 
 const Header = (props: HeaderProps) => (
   <View>
-    <Text category='h6'>{props.title}</Text>
-    <Text category='s1'>{props.subtitle}</Text>
+    <Text category='h1'>{props.title}</Text>
+    <Text category='label'>{props.subtitle}</Text>
   </View>
 );
+
+const NumberOfPlants = (plantsNumber: number) => {
+  if (plantsNumber === 0) {
+    return (
+      <View style={styles.controlContainer}>
+        <Text status="control" style={styles.textBold}>Start planting right now!</Text>
+      </View>
+    )
+  } else {
+    return <Text><Text status="info" style={styles.textBold}>{plantsNumber}</Text> plants!</Text>
+  }
+}
 
 /**
  * Greenhouse card that shows useful information
  */
 export const GreenhouseCard = observer(function GreenhouseCard(props: GreenhouseCardProps) {
+  const isOkay = props.greenhouse.isOkay
+  
   return (
     <React.Fragment>
       <Layout style={[props.style, styles.container]} level='3'>
         <Card
-          style={styles.card} status={props.greenhouse.isOkay === true ? 'success' : 'warning'}
+          style={styles.card} status={isOkay === true ? 'success' : 'warning'}
           header={<Header title={props.greenhouse.name} subtitle={props.greenhouse.description} />}
         >
-          <Text>Hello bello</Text>
+          { /* CHECK NUMBER OF PLANTS PLANTED */ }
+          <View style={styles.inlineIcon}>
+            <Icon style={styles.icon} name='cube' fill='#8F9BB3'/>
+            {NumberOfPlants(props.greenhouse.plants.length)}
+          </View>
+
+          { /* SHOW GREENHOUSE STATUS */ }
+          <View style={styles.inlineIcon}>
+            <Icon style={styles.icon} name='thermometer' fill='#8F9BB3'/>
+            <Text style={styles.textElement}>Status: 
+              {isOkay ? 
+                <Text status="info"  style={styles.textBold}> OKAY</Text> : <Text status="danger" style={styles.textBold}> PROBLEM DETECTED</Text>
+              }
+            </Text>
+          </View>
         </Card>
       </Layout>
     </React.Fragment>
   )
 })
 
+const controlBlue = '#3366FF'
 const styles = StyleSheet.create({
   card: {
     flex: 1,
@@ -47,6 +76,28 @@ const styles = StyleSheet.create({
   },
   container: {
     flexDirection: 'row',
-    flexWrap: 'wrap'
+    flexWrap: 'wrap',
   },
+  controlContainer: {
+    backgroundColor: controlBlue,
+    borderRadius: 4,
+    padding: 4,
+  },
+  icon: {
+    height: 24,
+    marginRight: 10,
+    width: 24
+  },
+  inlineIcon: {
+    alignItems: "center",
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    textAlignVertical: "center"
+  },
+  textBold: {
+    fontWeight: 'bold'
+  },
+  textElement: {
+    marginTop: 10
+  }
 });
