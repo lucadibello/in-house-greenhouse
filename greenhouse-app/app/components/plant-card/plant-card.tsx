58d2d123d95ab7fccd438c509d6acb4f4955e9e3
luca.dibello@student.supsi.ch <info@lucadibello.ch>
2022-03-13 16:33:20 +0100

    Updated plant card design

--- greenhouse-app/app/components/plant-card/plant-card.tsx
+++ greenhouse-app/app/components/plant-card/plant-card.tsx
@@ -16,8 +16,8 @@ interface HeaderProps {
 
 const Header = (props: HeaderProps) => (
   <View style={styles.cardHeader}>
-    <Text category='h3'>{props.title}</Text>
-    <Text category='label'>{props.subtitle}</Text>
+    <Text category='h6'>{props.title}</Text>
+    <Text category='label' style={styles.cardLabel}>- {props.subtitle}</Text>
   </View>
 );
 
@@ -36,6 +36,7 @@ export const PlantCard = observer(function (props: PlantCardProps) {
         <Card
           style={styles.card}
           header={<Header title={props.plant.name} subtitle={props.plant.description} />}
+          
         >
           <View style={styles.cardContainer}>
             {/* Planted date */}
@@ -43,18 +44,24 @@ export const PlantCard = observer(function (props: PlantCardProps) {
               Planted on: <Text category="c2">{startDate.toLocaleDateString()}</Text>
             </Text>
 
+            <View style={styles.spacer}></View>
+
             {/* Lifespan of the plant */}
             <Text category="h6">
               Lifespan: <Text style={styles.textBold}>{secondsDiff} hours</Text>
             </Text>
+
+            <View style={styles.spacer}></View>
 
             {/* Soil umidity */}
             <Text category="h6">
               Soil humidity: <Text style={styles.textBold}>10%</Text>
             </Text>
 
-            { /* Access plant data history */}
-            <Button>View plant history</Button>
+            <View style={styles.spacer}></View>
+
+              { /* Access plant data history */}
+              <Button status="info">View plant history</Button>
           </View>
         </Card>
       </Layout>
@@ -73,12 +80,21 @@ const styles = StyleSheet.create({
     textAlignVertical: "center"
   },
   cardHeader: {
+    alignItems: "center",
+    flexDirection: "row",
     margin: 2,
     padding: 5
   },
+  cardLabel: {
+    marginLeft: 5
+  }, 
   container: {
     flexDirection: 'row',
     flexWrap: 'wrap'
+  },
+  spacer: {
+    marginBottom: 5,
+    marginTop: 5
   },
   textBold: {
     fontWeight: 'bold'
