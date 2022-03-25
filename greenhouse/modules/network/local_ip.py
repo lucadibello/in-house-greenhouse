125be0a28b9c3d16971307a5acfa6429843a63ef
luca.dibello@student.supsi.ch <info@lucadibello.ch>
2022-03-25 20:05:30 +0100

    Created module to get local ip address

---
+++ greenhouse/modules/network/local_ip.py
@@ -0,0 +1,17 @@
+import socket 
+
+class LocalIp:
+  
+  @staticmethod
+  def get_local_ip ():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.settimeout(0)
+    try:
+      # doesn't even have to be reachable
+      s.connect(('10.255.255.255', 1))
+      IP = s.getsockname()[0]
+    except Exception:
+      IP = '127.0.0.1'
+    finally:
+      s.close()
+    return IP
