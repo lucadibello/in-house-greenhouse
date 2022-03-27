16b52e53bbec4c5f485d4e6542e944d607db4dc8
luca.dibello@student.supsi.ch <info@lucadibello.ch>
2022-03-27 19:55:11 +0200

    Added drawer open button

--- greenhouse-app/app/screens/homepage/homepage-screen.tsx
+++ greenhouse-app/app/screens/homepage/homepage-screen.tsx
@@ -2,13 +2,14 @@ import React, { FC, useEffect } from "react"
 import { Alert, SafeAreaView, StyleSheet } from "react-native"
 import { observer } from "mobx-react-lite"
 import { StackScreenProps } from "@react-navigation/stack"
-import { TabParamList, NavigatorParamList } from "../../navigators"
+import { TabParamList, NavigatorParamList, DrawerParamList } from "../../navigators"
 import { Divider, Icon, Layout, TopNavigation, TopNavigationAction } from "@ui-kitten/components"
 import { GreenhouseList } from "../../components"
 import { useStores } from "../../models"
 import { Greenhouse } from "../../models/greenhouse/greenhouse"
+import { DrawerScreenProps } from "@react-navigation/drawer"
 
-export const HomepageScreen: FC<StackScreenProps<(TabParamList & NavigatorParamList), "homepage">> = observer(
+export const HomepageScreen: FC<StackScreenProps<(TabParamList & NavigatorParamList), "homepage"> & DrawerScreenProps<DrawerParamList>> = observer(
   ({navigation}) => {
     
     // Load greenhouses from store
@@ -31,7 +32,7 @@ export const HomepageScreen: FC<StackScreenProps<(TabParamList & NavigatorParamL
           alignment='center'
           title={"Greenhouses"}
           subtitle='Your personal greenhouses'
-          accessoryLeft={<TopNavigationAction icon={<Icon name='menu'/>} onPress={() => Alert.alert("Implement drawer for account")} />}
+          accessoryLeft={<TopNavigationAction icon={<Icon name='menu'/>} onPress={() => navigation.openDrawer()} />}
           accessoryRight={<TopNavigationAction icon={<Icon name='plus'/>} onPress={() => navigation.navigate("scan")} />}
         />
         <Divider />
