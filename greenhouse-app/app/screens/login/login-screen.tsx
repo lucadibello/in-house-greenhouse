e92dd7c5d181b76c1b38d469248b15f5433db767
luca.dibello@student.supsi.ch <info@lucadibello.ch>
2022-03-28 22:42:34 +0200

    Created login screen

--- greenhouse-app/app/screens/login/login-screen.tsx
+++ greenhouse-app/app/screens/login/login-screen.tsx
@@ -1,44 +1,117 @@
 import React, { FC } from "react"
 import { observer } from "mobx-react-lite"
-import { StyleSheet } from "react-native"
+import { KeyboardAvoidingView, StyleSheet, View } from "react-native"
 import { StackScreenProps } from "@react-navigation/stack"
-import { SafeAreaView } from "react-native-safe-area-context"
-import { TopNavigation, Divider, Layout, Text } from "@ui-kitten/components"
-import { DrawerParamList } from "../../navigators/components/navigators"
+import { Input, Layout, Text, Button, Icon } from "@ui-kitten/components"
+import { AuthStackParamList } from "../../navigators/components/navigators"
+import { palette } from "../../theme/palette"
+import { TouchableOpacity } from "react-native-gesture-handler"
 
-export const LoginScreen: FC<StackScreenProps<DrawerParamList, "login">> = observer(
+export const LoginScreen: FC<StackScreenProps<AuthStackParamList , "login">> = observer(
   ({navigation}) => {
-    return (
-      <SafeAreaView>
-        <TopNavigation
-          alignment='center'
-          title={"Login"}
-          subtitle='Your personal greenhouses'
-        />
-        <Divider />
 
-        <Layout style={styles.container}>
-          <Text>Hello! login here</Text>
+    // show password state
+    const [showPassword, setShowPassword] = React.useState(false)
+
+    return (
+      <KeyboardAvoidingView style={styles.container}>
+        {/* Header */}
+        <View style={styles.headerContainer}>
+          <Text
+            category='h1'
+            status='control'
+          >
+            Hello!
+          </Text>
+          <Text
+            category='s1'
+            status='control'
+          >
+            Sign in to your account
+          </Text>
+        </View>
+
+        {/* Form */}
+        <Layout style={styles.formContainer}>
+          {/* E-mail input */}
+          <Input 
+            placeholder='E-Mail'
+            accessoryRight={<Icon name='cube' fill='#8F9BB3'/>}
+
+            style={styles.input}
+          />
+
+          {/* Password input */}
+          <Input
+            placeholder='Password'
+            passwordRules="required: upper; required: lower; required: digit; max-consecutive: 2; minlength: 8; maxlength: 16"
+            secureTextEntry={!showPassword}
+            accessoryRight={
+              <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
+                <Icon name={showPassword ? 'eye-outline' : 'eye-off-outline'} fill='#8F9BB3' />
+              </TouchableOpacity>
+            }
+            style={styles.input}
+          />
+
+          {/* Forgot password button */}
+          <Button style={styles.input} appearance='ghost' status='info' >
+            Forgot your password?
+          </Button>
+
+          {/* Footer */}
+          <Layout style={styles.footerContainer}>
+            {/* Login button */}
+            <Button style={styles.input} appearance='filled' status='primary' >
+              Login
+            </Button>
+
+            {/* Sign in button */}
+            <Button style={styles.input} appearance='ghost' status='basic' onPress={() => navigation.navigate("signup")} >
+              Don't have an account? Sign Up
+            </Button>
+          </Layout>
         </Layout>
-      </SafeAreaView>
+      </KeyboardAvoidingView>
     )
 })
 
-const notchColor = '#FFF'
 const styles = StyleSheet.create({
+  alignedBottom: {
+    alignItems: 'flex-end',
+    flex: 1,
+  },
   container: {
     flex: 1,
-    paddingLeft: "3%",
-    paddingRight: "3%"
   },
-  greenhouseList: {
+  footerContainer: {
     flex: 1,
-    marginBottom: 20
+    justifyContent: 'flex-end',
   },
-  notch: {
-    backgroundColor: notchColor,
+  forgotPassword: {
+    alignContent: 'flex-end'
   },
-  title: {
-    marginBottom: 2,
+  forgotPasswordContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+  },
+  formContainer: {
+    alignContent: 'space-between',
+    flex: 1,
+    flexDirection: 'column',
+    padding: 10
+  },
+  headerContainer: {
+    alignItems: 'center',
+    backgroundColor: palette.primaryBlue,
+    justifyContent: 'center',
+    minHeight: 400,
+  },
+  input: {
+    margin: 10
+  },
+  loginButton: {
+    padding: 10
   }
 })
