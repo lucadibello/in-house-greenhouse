941095077a26a20af7162cbe294d98cf12cc5746
luca.dibello@student.supsi.ch <info@lucadibello.ch>
2022-04-02 19:50:00 +0200

    Authentication check

--- greenhouse-app/app/services/api/authentication/authentication-api.ts
+++ greenhouse-app/app/services/api/authentication/authentication-api.ts
@@ -1,7 +1,7 @@
 import { ApiResponse } from "apisauce"
 import { Api } from "../core/api"
 import { getGeneralApiProblem } from "../core/problem/api-problem"
-import { LoginResult } from "../core/types/api.types"
+import { AuthenticationResult } from "../core/types/api.types"
 
 export class AuthenticationApi {
   private api: Api
@@ -10,7 +10,7 @@ export class AuthenticationApi {
     this.api = api
   }
 
-  async login(email: string, password: string): Promise<LoginResult> {
+  async login(email: string, password: string): Promise<AuthenticationResult> {
     try {
       // make the api call
       const response: ApiResponse<any> = await this.api.apisauce.post(`/login`,{
@@ -39,6 +39,68 @@ export class AuthenticationApi {
 
       // Read API response
       const userApiResponse = response.data.data.loginUser
+      
+      // Check if authentication error
+      if (userApiResponse.isError) {
+        // Return login error with error message
+        return {
+          kind: "not-ok",
+          token: "",
+          refreshToken: "",
+          user: null,
+          isError: true,
+          errorCode: userApiResponse.errorCode,
+          errorMessage: userApiResponse.errorMessage
+        }
+      } else {
+        // Return data
+        return {
+          kind: "ok",
+          token: userApiResponse.token,
+          refreshToken: userApiResponse.refreshToken,
+          user: null,
+          isError: true,
+          errorCode: userApiResponse.errorCode,
+          errorMessage: userApiResponse.errorMessage
+        }
+      }
+    } catch (e) {
+      __DEV__ && console.tron.log(e.message)
+      return { kind: "bad-data" }
+    }
+  }
+
+  async register (name: string, surname: string, email: string, password: string): Promise<any> {
+    try {
+      // make the api call
+      const response: ApiResponse<any> = await this.api.apisauce.post(`/register`,{
+        query: `query RegisterUser($email: String!, $name: String!, $surname: String!, $password: String!) {
+          registerUser(email: $email, name: $name, surname: $surname, password: $password) {
+            token
+            isError
+            errorCode
+            errorMessage
+            refreshToken
+          }
+        }`,
+        variables: {
+          name: name,
+          surname: surname,
+          email: email,
+          password: password
+        }
+      })
+
+      // the typical ways to die when calling an api
+      if (!response.ok) {
+        const problem = getGeneralApiProblem(response)
+        if (problem) {
+          return problem
+        }
+      }
+
+      // Read API response
+      const userApiResponse = response.data.data.registerUser
       
       // Check if authentication error
       if (userApiResponse.isError) {
