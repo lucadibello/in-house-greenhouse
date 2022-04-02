63eefd7c3c62578a2190b5c33d25d4eccab87446
luca.dibello@student.supsi.ch <info@lucadibello.ch>
2022-04-02 19:49:21 +0200

    Now password validation component is exported

--- greenhouse-app/app/components/index.ts
+++ greenhouse-app/app/components/index.ts
@@ -1,3 +1,4 @@
 export * from "./greenhouse-card/greenhouse-card"
 export * from "./greenhouse-list/greenhouse-list"
 export * from "./plant-card/plant-card"
+export * from "./password-validation/password-validation"
