e3dfc149252c51ba602e9c281d75b4a6829f3924
luca.dibello@student.supsi.ch <info@lucadibello.ch>
2022-04-02 19:47:19 +0200

    Now context contain request information

--- greenhouse-server/src/api/context.ts
+++ greenhouse-server/src/api/context.ts
@@ -1,11 +1,14 @@
 import { PrismaClient } from '@prisma/client'
+import { ExpressContext } from 'apollo-server-express'
 
 export interface Context {
-  prisma: PrismaClient
+  prisma: PrismaClient,
+  req: ExpressContext
 }
 
 const prisma = new PrismaClient()
 
-export const context: Context = {
+export const context = (req: ExpressContext): Context => ({
   prisma: prisma,
-}
+  req: req
+})
