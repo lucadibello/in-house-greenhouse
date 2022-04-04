4851a59049eef7b1e7a96a511005e736c2083f7a
luca.dibello@student.supsi.ch <info@lucadibello.ch>
2022-04-04 20:39:23 +0200

    Added method to check if GraphQL response is an AuthenticationError exception

--- greenhouse-app/app/services/api/core/base/ApiBase.ts
+++ greenhouse-app/app/services/api/core/base/ApiBase.ts
@@ -1,5 +1,14 @@
+import { ApiResponse } from "apisauce";
 import { Api } from "../api";
 
 export abstract class ApiBase {
   abstract api: Api
+
+
+  isAuthenticationErrorResponse (apiResponse: ApiResponse<any, any>): boolean {
+    if (apiResponse.data.errors !== undefined && apiResponse.data.errors.length > 0) {
+      return apiResponse.data.errors[0].extensions.code === "UNAUTHENTICATED"
+    }
+    return false
+  }
 }
