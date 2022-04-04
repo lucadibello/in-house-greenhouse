7a7157d89051b5a240f88b047e30877ed51019f8
luca.dibello@student.supsi.ch <info@lucadibello.ch>
2022-04-04 21:54:23 +0200

    Removed extra line

--- greenhouse-app/app/services/api/core/base/ApiBase.ts
+++ greenhouse-app/app/services/api/core/base/ApiBase.ts
@@ -4,7 +4,6 @@ import { Api } from "../api";
 export abstract class ApiBase {
   abstract api: Api
 
-
   isAuthenticationErrorResponse (apiResponse: ApiResponse<any, any>): boolean {
     if (apiResponse.data.errors !== undefined && apiResponse.data.errors.length > 0) {
       return apiResponse.data.errors[0].extensions.code === "UNAUTHENTICATED"
