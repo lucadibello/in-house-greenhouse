ef10dcd04251d45e45bc460abe1981056a287a84
luca.dibello@student.supsi.ch <info@lucadibello.ch>
2022-04-06 09:55:04 +0200

    Removed drawer items group + added navigation to Profile item

--- greenhouse-app/app/navigators/components/drawer-navigation.tsx
+++ greenhouse-app/app/navigators/components/drawer-navigation.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import { Drawer, Icon, DrawerGroup, DrawerItem } from '@ui-kitten/components';
+import { Drawer, Icon, DrawerGroup, DrawerItem, Divider } from '@ui-kitten/components';
 import { SafeAreaView, StyleSheet } from 'react-native';
 import { DrawerParamList } from './navigators';
 import { AppStackNavigation } from './stack-navigation';
@@ -8,6 +8,7 @@ import { useStores } from '../../models/root-store/root-store-context';
 import { observer } from 'mobx-react-lite';
 import { UserDetails } from '../../components';
 import { User } from '../../models';
+import { ProfileScreen } from '../../screens';
 
 const HomeDrawer = createDrawerNavigator<DrawerParamList>();
 
@@ -22,20 +23,23 @@ const DrawerContent = (props: { navigation, state, logoutUser: () => void, user:
     <SafeAreaView>
       {/* User details */}
       <UserDetails style={styles.userDetails} user={props.user} onUserPress={props.userDetailsClicked} />
+
       {/* Drawer options */}
       <Drawer>
           <DrawerItem title='Homepage' 
             accessoryLeft={<Icon name='home' fill='#8F9BB3'/>}
             onPress={() => props.navigation.navigate("homepage")} 
           />
-          <DrawerGroup title='Profile' accessoryLeft={<Icon name='person' fill='#8F9BB3'/>}>
-            <DrawerItem title='Manage profile' accessoryLeft={<Icon name='edit-2-outline' fill='#8F9BB3'/>}/>
-            <DrawerItem
-              title='Logout'
-              onPress={props.logoutUser}
-              accessoryLeft={<Icon name='log-out-outline' fill='#8F9BB3'/>}
-            />
-          </DrawerGroup>
+          <DrawerItem
+            title='Profile'
+            accessoryLeft={<Icon name='person' fill='#8F9BB3'/>}
+            onPress={() => props.navigation.navigate("profile")}
+          />
+          <DrawerItem
+            title='Logout'
+            onPress={props.logoutUser}
+            accessoryLeft={<Icon name='log-out-outline' fill='#8F9BB3'/>}
+          />
       </Drawer>
     </SafeAreaView>
   </React.Fragment>
@@ -69,6 +73,7 @@ export const AppDrawerNavigator = observer(() => {
       }}
     >
       <HomeDrawer.Screen name="homepage" component={AppStackNavigation} />
+      <HomeDrawer.Screen name="profile" component={ProfileScreen} />
     </HomeDrawer.Navigator>
   )
 })
