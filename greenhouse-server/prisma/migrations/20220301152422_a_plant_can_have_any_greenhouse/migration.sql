1b9bd33b19f807c48de8cdd88cb70e308d7938ac
luca.dibello@student.supsi.ch <info@lucadibello.ch>
2022-04-08 18:54:50 +0200

    Deleted all old migrations

--- greenhouse-server/prisma/migrations/20220301152422_a_plant_can_have_any_greenhouse/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE "Plant" DROP CONSTRAINT "Plant_greenhouseId_fkey";
-
--- AlterTable
-ALTER TABLE "Plant" ALTER COLUMN "greenhouseId" DROP NOT NULL;
-
--- AddForeignKey
-ALTER TABLE "Plant" ADD CONSTRAINT "Plant_greenhouseId_fkey" FOREIGN KEY ("greenhouseId") REFERENCES "Greenhouse"("id") ON DELETE SET NULL ON UPDATE CASCADE;
--- greenhouse-server/prisma/migrations/20220324154413_added_is_deleted_flag_on_plant_model/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Plant" ADD COLUMN     "isDeleted" BOOLEAN NOT NULL DEFAULT false;
