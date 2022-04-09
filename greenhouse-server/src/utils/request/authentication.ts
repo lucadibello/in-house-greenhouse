bcdf3ab2c2b51802efd8c2e641cc666bf0981a6d
luca.dibello@student.supsi.ch <info@lucadibello.ch>
2022-04-09 17:29:27 +0200

    Updated authentication route

--- greenhouse-server/src/utils/request/authentication.ts
+++ greenhouse-server/src/utils/request/authentication.ts
@@ -2,6 +2,7 @@ import { ExpressContext } from "apollo-server-express";
 
 import { JWT, TokenType } from '../../utils/jwt/jwt'
 import { getAccessTokenSecret, getRefreshTokenSecret } from '../../utils/env/env'
+import { TokenExpiredError } from "jsonwebtoken";
 
 // initialize JWT class using .env variables
 const jwtService = new JWT(
@@ -10,16 +11,25 @@ const jwtService = new JWT(
 )
 
 export const isLoggedIn = (req: ExpressContext): boolean => {
-  // get request authorization header
-  const authorization = req.req.headers.authorization || '';
+  try {
+    // get request authorization header
+    const authorization = req.req.headers.authorization || '';
 
-  // Verify token using JWT object
-  const isValid = jwtService.verifyToken(authorization, TokenType.TOKEN_ACCESS)
+    // Verify token using JWT object
+    const isValid = jwtService.verifyToken(authorization, TokenType.TOKEN_ACCESS)
 
-  // isValid is null if token is invalid, if it's a User object it means the token is valid
-  if (isValid !== null) {
-    return true
-  } else {
-    return false
+    // isValid is null if token is invalid, if it's a User object it means the token is valid
+    if (isValid !== null) {
+      return true
+    } else {
+      return false
+    }
+  } catch (error) {
+    if (error instanceof TokenExpiredError) {
+      return false
+    } else {
+      console.log(error)
+      return false;
+    }
   }
 }
