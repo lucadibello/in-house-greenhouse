379342e3a6647e3dde4e3f32124d447df5fc8f3a
luca.dibello@student.supsi.ch <info@lucadibello.ch>
2022-04-11 20:18:40 +0200

    Proxy now sends request to API server and return response to original caller

--- greenhouse-iot/GreenProxy/proxy/proxy.go
+++ greenhouse-iot/GreenProxy/proxy/proxy.go
@@ -1,16 +1,18 @@
 package proxy
 
 import (
+	"bytes"
 	"fmt"
 	"io"
+	"io/ioutil"
 
 	"greenproxy/authentication"
 	"net/http"
 )
 
 type Proxy struct {
-	ProxyRoute             string
-	AuthenticationEndpoint string
+	ProxyRoute  string
+	ApiEndpoint string
 }
 
 var authenticationStatus authentication.AuthenticationResult = authentication.AuthenticationResult{
@@ -20,39 +22,61 @@ var authenticationStatus authentication.AuthenticationResult = authentication.Au
 
 // Handle an API request
 func ProxyRequest(w http.ResponseWriter, r *http.Request, settings Proxy) {
+	// Check if user has sent X-Greenhouse-UUID header
+	if r.Header.Get("X-Greenhouse-UUID") == "" {
+		// User has not sent X-Greenhouse-UUID header
+		fmt.Println("[Proxy] User has not sent X-Greenhouse-UUID header")
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
 	// Check if user has already a token saved inside global variable
 	// If not, fetch a new one
 	if !authenticationStatus.Success || authenticationStatus.Token == "" {
 		// Save new status
-		authenticationStatus = authentication.Authenticate(settings.AuthenticationEndpoint, "")
+		authenticationStatus = authentication.Authenticate(settings.ApiEndpoint, r.Header.Get("X-Greenhouse-UUID"))
 	}
 
 	// Check if authentication was successful
 	if authenticationStatus.Success {
-		// Set token to request header
-		r.Header.Set("Authorization", authenticationStatus.Token)
-		// Set content type to application/json
-		r.Header.Set("Content-Type", "application/json")
-		// Send request with modifed header to greenhouse APIs
-		response, err := http.DefaultClient.Do(r)
+		// Create new request to API using same settings as the incoming one
+		buf, err := ioutil.ReadAll(r.Body)
 
-		// Return response to client
-		if err == nil {
-			io.Copy(w, response.Body)
-		} else {
-			fmt.Println("Error while fetching response:", err)
+		if err != nil {
+			fmt.Println("[Proxy] Error while reading request body:", err)
+			io.WriteString(w, "Error while reading request body")
+			w.WriteHeader(http.StatusInternalServerError)
+			return
 		}
+
+		// Copy body from old request
+		bodyCopy := io.NopCloser(bytes.NewBuffer(buf))
+
+		// Create new request to API with same parameters but different endpoint
+		newRequest, _ := http.NewRequest(r.Method, settings.ApiEndpoint, bodyCopy)
+
+		// Set token inside header
+
+		fmt.Println("[Proxy] New authentication token: " + authenticationStatus.Token)
+		newRequest.Header.Set("Authorization", authenticationStatus.Token)
+		newRequest.Header.Set("Content-Type", "application/json")
+		fmt.Println(newRequest)
+
+		// Send request
+		resp, err := http.DefaultClient.Do(newRequest)
+
+		if err != nil {
+			fmt.Println("Error while sending request:", err)
+			return
+		}
+		// Copy response to the client
+		io.Copy(w, resp.Body)
 	} else {
-		io.WriteString(w, "Authentication failed")
+		io.WriteString(w, "Authentication failed: "+authenticationStatus.ErrorMessage)
 	}
 }
 
 func StartProxy(route string, port int, proxySettings Proxy) error {
-	// print proxy settings
-	fmt.Println("Proxy settings:")
-	fmt.Println("Proxy route:", proxySettings.ProxyRoute)
-	fmt.Println("Authentication endpoint:", proxySettings.AuthenticationEndpoint)
-
 	// Add API handler with config
 	http.Handle(proxySettings.ProxyRoute, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		ProxyRequest(w, r, proxySettings)
