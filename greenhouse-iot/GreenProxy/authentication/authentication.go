5836a7116dc5fc751a65c921cd98e11c4a736190
luca.dibello@student.supsi.ch <info@lucadibello.ch>
2022-04-11 20:17:08 +0200

    Authenticate method now uses machinebox/graphql

--- greenhouse-iot/GreenProxy/authentication/authentication.go
+++ greenhouse-iot/GreenProxy/authentication/authentication.go
@@ -1,36 +1,79 @@
 package authentication
 
 import (
+	"context"
 	"fmt"
-	"net/http"
+
+	"github.com/machinebox/graphql"
 )
 
 // Authentication struct
 type AuthenticationResult struct {
-	Success bool
-	Token   string
+	Success      bool
+	Token        string
+	ErrorMessage string
+}
+
+type GraphQLResponse struct {
+	GreenhouseAuth struct {
+		Token        string `json:"token"`
+		Expire       string `json:"expire"`
+		Issued       string `json:"issued"`
+		ErrorCode    string `json:"errorCode"`
+		ErrorMessage string `json:"errorMessage"`
+	} `json:"greenhouseAuth"`
 }
 
 // Authenticate function
 func Authenticate(endpoint string, UUID string) AuthenticationResult {
 	fmt.Println("Fetching temporary token for operation on greenhouse ", UUID)
 
-	// Send request to endpoint with data
-	response, err := http.Post(endpoint, "application/json", nil)
+	// Create graphql client
+	graphqlClient := graphql.NewClient(endpoint)
+	graphqlRequest := graphql.NewRequest(`
+		query authentication($greenhouseId: String!) {
+			greenhouseAuth(greenhouseId: $greenhouseId) {
+				token
+				expire
+				issued
+				errorCode
+				errorMessage
+			}
+		}
+	`)
 
-	// Read response
+	// Set usefull headers
+	graphqlRequest.Header.Set("Cache-Control", "no-cache")
 
-	// Check if error
-	if err == nil {
-		// Print response
-		fmt.Println("Authentication response:", response)
+	// Add UUID variable to request
+	graphqlRequest.Var("greenhouseId", UUID)
 
-		// Return success status with token
-		return AuthenticationResult{true, ""}
+	// Send request
+	var graphqlResponse GraphQLResponse
+	ctx := context.Background()
+	if err := graphqlClient.Run(ctx, graphqlRequest, &graphqlResponse); err != nil {
+		// Graphql request failed
+		return AuthenticationResult{
+			Success:      false,
+			Token:        "",
+			ErrorMessage: err.Error(),
+		}
+	}
+
+	// Check GraphQL response status
+	if graphqlResponse.GreenhouseAuth.ErrorCode != "" {
+		// GraphQL returned an error
+		return AuthenticationResult{
+			Success:      false,
+			Token:        "",
+			ErrorMessage: graphqlResponse.GreenhouseAuth.ErrorMessage,
+		}
 	} else {
-		fmt.Println("Error while fetching temporary token:", err)
-		panic(err)
-		// Error found
-		return AuthenticationResult{false, ""}
+		// GraphQL returned a token
+		return AuthenticationResult{
+			Success:      true,
+			Token:        graphqlResponse.GreenhouseAuth.Token,
+			ErrorMessage: "",
+		}
 	}
 }
