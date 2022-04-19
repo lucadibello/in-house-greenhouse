b232f63d75b1e0bba4c30dd2dbe724f5d1d52cb3
luca.dibello@student.supsi.ch <info@lucadibello.ch>
2022-04-19 18:26:07 +0200

    Added position resolved field

--- greenhouse-server/src/api/graphql/Sensor.ts
+++ greenhouse-server/src/api/graphql/Sensor.ts
@@ -2,13 +2,22 @@ import { AuthenticationError } from "apollo-server"
 import { extendType, list, objectType } from "nexus"
 import { isLoggedIn } from "../../utils/request/authentication"
 import { PositionType, SensorType } from "./enums"
+import { Position } from "./Position"
 
 export const Sensor = objectType({
   name: 'Sensor',
   definition(t) {
     t.nonNull.string('name', { description: "Sensor name" }),
     t.nonNull.field('type', { type: SensorType , description: "Sensor type"}),
-    t.nonNull.field('position', { type: PositionType, description: "Sensor position"})
+    t.nonNull.field('positionType', { type: PositionType, description: "Sensor position"})
+    t.field('position', {
+      type: Position,
+      resolve: (parent, _, ctx) => {
+        return ctx.prisma.position.findUnique({
+          where: { type: parent.positionType },
+        })
+      },
+    });
   }
 })
 
