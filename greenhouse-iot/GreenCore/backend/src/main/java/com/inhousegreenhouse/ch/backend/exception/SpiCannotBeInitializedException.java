7eda57f0cd6ebdbf3b395b7512732d3db48969ef
luca.dibello@student.supsi.ch <info@lucadibello.ch>
2022-05-04 09:31:08 +0200

    Special kind of exception, thrown when the AdC SPI connection cannot be started

---
+++ greenhouse-iot/GreenCore/backend/src/main/java/com/inhousegreenhouse/ch/backend/exception/SpiCannotBeInitializedException.java
@@ -0,0 +1,7 @@
+package com.inhousegreenhouse.ch.backend.exception;
+
+public class SpiCannotBeInitializedException extends Exception {
+    public SpiCannotBeInitializedException(String message) {
+        super(message);
+    }
+}
