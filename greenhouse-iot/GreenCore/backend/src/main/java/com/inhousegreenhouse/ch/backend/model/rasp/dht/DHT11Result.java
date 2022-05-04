a9d048e99f249bdd23d6d2a74b2b2b8073095de0
luca.dibello@student.supsi.ch <info@lucadibello.ch>
2022-05-04 09:30:40 +0200

    This class hold a DHT11 response and a little report to show how many requests have been sent to the device

---
+++ greenhouse-iot/GreenCore/backend/src/main/java/com/inhousegreenhouse/ch/backend/model/rasp/dht/DHT11Result.java
@@ -0,0 +1,43 @@
+package com.inhousegreenhouse.ch.backend.model.rasp.dht;
+
+public class DHT11Result {
+    // DHT11 sensor result returned by DHT11.read() method
+
+    public static final int ERR_NO_ERROR = 0;
+    public static final int ERR_MISSING_DATA = 1;
+    public static final int ERR_CRC = 2;
+
+    private final int errorCode;
+    private final double temperature;
+    private final double humidity;
+    private final int listElements;
+    private final int detectedBits;
+
+    public DHT11Result(int errorCode, double temperature, double humidity, int listElements, int detectedBits) {
+        this.errorCode   = errorCode;
+        this.temperature = temperature;
+        this.humidity    = humidity;
+
+        this.listElements = listElements;
+        this.detectedBits = detectedBits;
+    }
+
+    public boolean isValid() {
+        return errorCode == ERR_NO_ERROR;
+    }
+    public int getErrorCode() {
+        return errorCode;
+    }
+    public double getTemperature() {
+        return temperature;
+    }
+    public double getHumidity() {
+        return humidity;
+    }
+    double getListElements() {
+        return listElements;
+    }
+    double getDetectedBits() {
+        return detectedBits;
+    }
+}
