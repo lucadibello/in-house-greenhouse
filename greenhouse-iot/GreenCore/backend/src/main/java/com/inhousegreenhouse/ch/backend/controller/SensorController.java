c7083c7c47aba61c836655dbbc63f3861869c804
luca.dibello@student.supsi.ch <info@lucadibello.ch>
2022-05-04 09:40:09 +0200

    Now the sensor controller creates automatically an ADC object, and pass that to the lower layer

--- greenhouse-iot/GreenCore/backend/src/main/java/com/inhousegreenhouse/ch/backend/controller/SensorController.java
+++ greenhouse-iot/GreenCore/backend/src/main/java/com/inhousegreenhouse/ch/backend/controller/SensorController.java
@@ -1,16 +1,25 @@
 package com.inhousegreenhouse.ch.backend.controller;
 
-import com.inhousegreenhouse.ch.backend.model.sensor.SensorList;
+import com.inhousegreenhouse.ch.backend.exception.SpiCannotBeInitializedException;
+import com.inhousegreenhouse.ch.backend.model.sensor.converter.ADC;
+import com.inhousegreenhouse.ch.backend.model.sensor.core.SensorList;
 import com.inhousegreenhouse.ch.backend.model.util.Greenhouse;
 import com.inhousegreenhouse.ch.backend.repository.SensorRepository;
 import com.inhousegreenhouse.ch.backend.service.SensorService;
+
+import java.io.IOException;
 
 public class SensorController {
 
     private final SensorService sensorService;
 
-    public SensorController (Greenhouse greenhouse) {
-        this.sensorService = new SensorService(new SensorRepository(greenhouse));
+    public SensorController (Greenhouse greenhouse) throws SpiCannotBeInitializedException {
+        try {
+            ADC adc = new ADC();
+            this.sensorService = new SensorService(new SensorRepository(greenhouse, adc));
+        } catch (IOException e) {
+            throw new SpiCannotBeInitializedException("SPI cannot be initialized: " + e.getMessage());
+        }
     }
 
     public SensorList getSensors () {
