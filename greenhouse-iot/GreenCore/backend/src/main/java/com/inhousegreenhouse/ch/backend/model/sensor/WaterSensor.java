ed803689d2dac157b4f07dff3cc1ce7347a53bed
luca.dibello@student.supsi.ch <info@lucadibello.ch>
2022-05-04 09:36:19 +0200

    Now WaterSensor is an AnalogSensor

--- greenhouse-iot/GreenCore/backend/src/main/java/com/inhousegreenhouse/ch/backend/model/sensor/WaterSensor.java
+++ greenhouse-iot/GreenCore/backend/src/main/java/com/inhousegreenhouse/ch/backend/model/sensor/WaterSensor.java
@@ -1,21 +1,29 @@
 package com.inhousegreenhouse.ch.backend.model.sensor;
 
+import com.inhousegreenhouse.ch.backend.model.sensor.converter.ADC;
+import com.inhousegreenhouse.ch.backend.model.sensor.core.AnalogSensor;
+import com.inhousegreenhouse.ch.backend.model.sensor.core.BaseSensor;
+import com.inhousegreenhouse.ch.backend.model.sensor.core.Position;
+
 /**
  * WaterSensor class defines the water sensor.
  */
-public class WaterSensor extends BaseSensor<Double> {
+public class WaterSensor extends AnalogSensor<Double> {
 
     /**
      * Constructor for WaterSensor.
      * @param name name of the sensor.
      * @param defaultValue default value of the sensor.
+     * @param adc ADC object.
+     * @param channel ADC channel where the sensor is connected to.
      */
-    public WaterSensor(String name, double defaultValue) {
-        super(name, Position.GENERAL, defaultValue);
+    public WaterSensor(String name, double defaultValue, ADC adc, int channel) {
+        super(name, Position.GENERAL, defaultValue, adc, channel);
     }
 
     @Override
-    public void updateValue() {
+    public Double updateAndGet() {
         this.value = Math.random();
+        return null;
     }
 }
