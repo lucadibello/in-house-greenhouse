e27fa73a92c0f011ea9ff4c61400d2750954e358
luca.dibello@student.supsi.ch <info@lucadibello.ch>
2022-05-04 10:48:02 +0200

    Added javadoc to WaterSensor

--- greenhouse-iot/GreenCore/backend/src/main/java/com/inhousegreenhouse/ch/backend/model/sensor/WaterSensor.java
+++ greenhouse-iot/GreenCore/backend/src/main/java/com/inhousegreenhouse/ch/backend/model/sensor/WaterSensor.java
@@ -1,5 +1,6 @@
 package com.inhousegreenhouse.ch.backend.model.sensor;
 
+import com.inhousegreenhouse.ch.backend.exception.SpiCannotBeInitializedException;
 import com.inhousegreenhouse.ch.backend.model.sensor.converter.ADC;
 import com.inhousegreenhouse.ch.backend.model.sensor.core.AnalogSensor;
 import com.inhousegreenhouse.ch.backend.model.sensor.core.BaseSensor;
@@ -14,16 +15,20 @@ public class WaterSensor extends AnalogSensor<Double> {
      * Constructor for WaterSensor.
      * @param name name of the sensor.
      * @param defaultValue default value of the sensor.
-     * @param adc ADC object.
-     * @param channel ADC channel where the sensor is connected to.
+     * @param adc the AdC object to use to convert analog signal.
+     * @param channel the ADC channel where the sensor is connected.
      */
     public WaterSensor(String name, double defaultValue, ADC adc, int channel) {
         super(name, Position.GENERAL, defaultValue, adc, channel);
     }
 
+    /**
+     * Updates the value of the sensor and returns it.
+     * @return the value of the sensor.
+     * @throws SpiCannotBeInitializedException if the SPI cannot be initialized.
+     */
     @Override
-    public Double updateAndGet() {
-        this.value = Math.random();
+    public Double updateAndGet() throws SpiCannotBeInitializedException {
         return null;
     }
 }
