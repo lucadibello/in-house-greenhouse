36e43faaaaa757c1d34c7fe3b35c83842d0aa13c
luca.dibello@student.supsi.ch <info@lucadibello.ch>
2022-05-04 09:27:03 +0200

    Moved classes inside new package

---
+++ greenhouse-iot/GreenCore/backend/src/main/java/com/inhousegreenhouse/ch/backend/model/sensor/core/ISensor.java
@@ -0,0 +1,31 @@
+package com.inhousegreenhouse.ch.backend.model.sensor.core;
+
+import com.inhousegreenhouse.ch.backend.exception.SpiCannotBeInitializedException;
+
+/**
+ * Common interface for all sensors.
+ */
+public interface ISensor<T extends Number> {
+    /**
+     * Returns the sensor's value.
+     * @return the sensor's value
+     */
+    T updateAndGet() throws SpiCannotBeInitializedException;
+
+    /**
+     * Returns the sensor's name.
+     * @return the sensor's name
+     */
+    String getName();
+
+    /**
+     * Returns sensor position.
+     * @return sensor position
+     */
+    Position getPosition();
+
+    /**
+     * Update sensor's value.
+     */
+    T getCachedValue();
+}
---
+++ greenhouse-iot/GreenCore/backend/src/main/java/com/inhousegreenhouse/ch/backend/model/sensor/core/SensorType.java
@@ -0,0 +1,7 @@
+package com.inhousegreenhouse.ch.backend.model.sensor.core;
+
+public enum SensorType {
+    HUMIDITY,
+    TEMPERATURE,
+    SOIL_MOISTURE
+}
