3074d8933932a604227dc3b1a5243440609aa3d2
luca.dibello@student.supsi.ch <info@lucadibello.ch>
2022-05-04 11:02:51 +0200

    Added JavaDoc to SensorObserver

--- greenhouse-iot/GreenCore/backend/src/main/java/com/inhousegreenhouse/ch/backend/orchestrator/SensorObserver.java
+++ greenhouse-iot/GreenCore/backend/src/main/java/com/inhousegreenhouse/ch/backend/orchestrator/SensorObserver.java
@@ -5,18 +5,41 @@ import com.inhousegreenhouse.ch.backend.exception.SpiCannotBeInitializedExceptio
 import com.inhousegreenhouse.ch.backend.model.api.SensorApi;
 import com.inhousegreenhouse.ch.backend.model.sensor.core.ISensor;
 
+/**
+ * The sensor observer is the interface that is used to observe the sensor.
+ * @param <T> The type of the sensor that will be observed.
+ */
 public class SensorObserver<T extends ISensor<?>> implements Runnable {
-
+    /**
+     * The monitoring configuration.
+     */
     private final MonitoringConfig config;
+
+    /**
+     * The sensor to observe.
+     */
     private final T sensor;
+
+    /**
+     * The observer thread identifier.
+     */
     private final String threadIdentifier;
 
+    /**
+     * Constructor.
+     * @param config The monitoring configuration.
+     * @param sensor The sensor to observe.
+     * @param threadIdentifier The thread identifier.
+     */
     public SensorObserver(MonitoringConfig config, T sensor, String threadIdentifier) {
         this.config = config;
         this.sensor = sensor;
         this.threadIdentifier = threadIdentifier;
     }
 
+    /**
+     * Start observing the sensor.
+     */
     @Override
     public void run() {
         System.out.println("[Thread Orchestrator] " +
@@ -48,6 +71,8 @@ public class SensorObserver<T extends ISensor<?>> implements Runnable {
 
     /**
      * Record data inside the database
+     * @throws ProxyRequestFailException If the request to the proxy fails.
+     * @throws SpiCannotBeInitializedException If the SPI cannot be initialized.
      */
     private void recordData () throws ProxyRequestFailException, SpiCannotBeInitializedException {
         // Create sensor api
