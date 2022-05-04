4c16f9fac9b730d3f269a480ba370282323965c2
luca.dibello@student.supsi.ch <info@lucadibello.ch>
2022-05-04 09:28:47 +0200

    ADC class used to read the AdC conversion result via SPI

---
+++ greenhouse-iot/GreenCore/backend/src/main/java/com/inhousegreenhouse/ch/backend/model/sensor/converter/ADC.java
@@ -0,0 +1,50 @@
+package com.inhousegreenhouse.ch.backend.model.sensor.converter;
+
+import com.pi4j.io.spi.SpiChannel;
+import com.pi4j.io.spi.SpiDevice;
+import com.pi4j.io.spi.SpiFactory;
+
+import java.io.IOException;
+
+/**
+ * This class represents the MCP3008 ADC of the Raspberry Pi.
+ */
+public class ADC {
+    private final SpiDevice spi;
+    public static short ADC_CHANNEL_COUNT = 8;
+
+    public ADC () throws IOException {
+        // Create SPI device
+        spi = SpiFactory.getInstance(
+            SpiChannel.CS0, // SPI channel selector output
+            SpiDevice.DEFAULT_SPI_SPEED, // Default SPI speed, set at 1 MHz
+            SpiDevice.DEFAULT_SPI_MODE // default SPI mode (set at 0)
+        );
+    }
+
+    /**
+     * Reads the value of the specified ADC channel.
+     * @return The value of the specified ADC channel.
+     */
+    public int read(int channel) throws IOException {
+        // Check if channel is valid, otherwise throw an exception.
+        if (channel < 0 || channel > ADC_CHANNEL_COUNT) {
+            throw new IllegalArgumentException("Invalid channel number");
+        }
+
+        // create a data buffer and initialize a conversion request payload
+        final byte[] data = new byte[] {
+                (byte) 0b00000001,                              // first byte, start bit
+                (byte)(0b10000000 |( ((channel & 7) << 4))),    // second byte transmitted -> (SGL/DIF = 1, D2=D1=D0=0)
+                (byte) 0b00000000                               // third byte transmitted....don't care
+        };
+
+        // send data to the MCP3008 via the SPI bus
+        byte[] result = spi.write(data);
+        // Print result length
+        System.out.println("Result length: " + result.length);
+
+        // Convert the result to an integer
+        return (result[2] & 0xFF) | ((result[1] & 0xFF) << 8) | ((result[0] & 0x0F) << 16);
+    }
+}
