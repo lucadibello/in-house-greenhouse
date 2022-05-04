69a2fafc28986828f3b65609f9c0976c4163a438
luca.dibello@student.supsi.ch <info@lucadibello.ch>
2022-05-04 10:47:51 +0200

    Added javadoc to TemperatureSensor

--- greenhouse-iot/GreenCore/backend/src/main/java/com/inhousegreenhouse/ch/backend/model/sensor/TemperatureSensor.java
+++ greenhouse-iot/GreenCore/backend/src/main/java/com/inhousegreenhouse/ch/backend/model/sensor/TemperatureSensor.java
@@ -14,6 +14,8 @@ public class TemperatureSensor extends AnalogSensor<Double> {
      * TemperatureSensor constructor.
      * @param name the name of the sensor.
      * @param value the value of the sensor.
+     * @param adc the AdC object to use to convert analog signal.
+     * @param channel the ADC channel where the sensor is connected.
      */
     public TemperatureSensor(String name, double value, ADC adc, int channel) {
         super(name, Position.GENERAL, value, adc, channel);
