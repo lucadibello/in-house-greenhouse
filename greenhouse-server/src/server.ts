f3540734e9c012fdbc6a9caf1978351ca9c70de8
luca.dibello@student.supsi.ch <info@lucadibello.ch>
2022-05-05 14:16:37 +0200

    Fixed bug: was using wrong .env key

--- greenhouse-server/src/server.ts
+++ greenhouse-server/src/server.ts
@@ -14,8 +14,8 @@ const server = new ApolloServer({
   }
 })
 // Check if API server url and port are set in .env, otherwise set default settings
-const port = process.env.PORT || 4000
-const url = process.env.API_URL || 'localhost'
+const port = process.env.API_SERVER_PORT || 4000
+const url = process.env.API_SERVER_URL || 'localhost'
 
 // Start listening
 server.listen({
