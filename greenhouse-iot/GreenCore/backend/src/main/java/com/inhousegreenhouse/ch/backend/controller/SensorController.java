0a0cf0fb670cc8055e20327cb4a75f7dec22c776
luca.dibello@student.supsi.ch <info@lucadibello.ch>
2022-05-11 01:18:42 +0200

    Now ADC channels are registered

--- greenhouse-iot/GreenCore/backend/src/main/java/com/inhousegreenhouse/ch/backend/controller/SensorController.java
+++ greenhouse-iot/GreenCore/backend/src/main/java/com/inhousegreenhouse/ch/backend/controller/SensorController.java
@@ -2,7 +2,10 @@ package com.inhousegreenhouse.ch.backend.controller;
 
 import com.inhousegreenhouse.ch.backend.exception.SpiCannotBeInitializedException;
 import com.inhousegreenhouse.ch.backend.model.sensor.converter.ADC;
+import com.inhousegreenhouse.ch.backend.model.sensor.core.AnalogSensor;
 import com.inhousegreenhouse.ch.backend.model.sensor.core.SensorList;
+import com.inhousegreenhouse.ch.backend.model.sensor.core.SensorType;
+import com.inhousegreenhouse.ch.backend.model.util.Channels;
 import com.inhousegreenhouse.ch.backend.model.util.Greenhouse;
 import com.inhousegreenhouse.ch.backend.repository.SensorRepository;
 import com.inhousegreenhouse.ch.backend.service.SensorService;
@@ -26,7 +29,15 @@ public class SensorController {
      */
     public SensorController (Greenhouse greenhouse) throws SpiCannotBeInitializedException {
         try {
+            // Initialize the SPI interface
             ADC adc = new ADC();
+
+            // Register the sensors inside the channel manager
+            AnalogSensor.channels.registerSensor(SensorType.SOIL_MOISTURE, 0, 5);
+            AnalogSensor.channels.registerSensor(SensorType.HUMIDITY, 6, 6);
+            AnalogSensor.channels.registerSensor(SensorType.TEMPERATURE, 7, 7);
+
+            // Initialize the sensor service
             this.sensorService = new SensorService(new SensorRepository(greenhouse, adc));
         } catch (IOException e) {
             throw new SpiCannotBeInitializedException("SPI cannot be initialized: " + e.getMessage());
