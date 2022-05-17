d800c666ee31fb473e8426a02d1e06fbcff461b4
luca.dibello@student.supsi.ch <info@lucadibello.ch>
2022-05-17 13:54:45 +0200

    Added CyclicBarrier to synchronize thread execution + watering system management

--- greenhouse-iot/GreenCore/backend/src/main/java/com/inhousegreenhouse/ch/backend/orchestrator/MonitoringOrchestrator.java
+++ greenhouse-iot/GreenCore/backend/src/main/java/com/inhousegreenhouse/ch/backend/orchestrator/MonitoringOrchestrator.java
@@ -1,11 +1,16 @@
 package com.inhousegreenhouse.ch.backend.orchestrator;
 
-import com.inhousegreenhouse.ch.backend.model.sensor.*;
-import com.inhousegreenhouse.ch.backend.model.sensor.core.SensorList;
+import com.inhousegreenhouse.ch.backend.model.greenhouse.sensor.HumiditySensor;
+import com.inhousegreenhouse.ch.backend.model.greenhouse.sensor.MoistureSensor;
+import com.inhousegreenhouse.ch.backend.model.greenhouse.sensor.TemperatureSensor;
+import com.inhousegreenhouse.ch.backend.model.greenhouse.sensor.core.SensorList;
+import com.inhousegreenhouse.ch.backend.model.greenhouse.watering.WateringSystem;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * Sensor observers orchestrator required to monitor the greenhouse sensors.
@@ -17,6 +22,11 @@ public class MonitoringOrchestrator {
     public static volatile boolean isRunning = true;
 
     /**
+     * Cyclic barrier, used to synchronize all threads.
+     */
+    private static CyclicBarrier barrier;
+
+    /**
      * List of sensors to monitor.
      */
     private final SensorList sensorList;
@@ -26,11 +36,20 @@ public class MonitoringOrchestrator {
      */
     private final List<Thread> threadPool = new ArrayList<>();
 
-
     /**
      * Settings for the greencore system.
      */
     private final Properties settings;
+
+    /**
+     * Minimum level of humidity to be considered as a dry sensor.
+     */
+    private final double minHumidity;
+
+    /**
+     * Duration of the watering cycle.
+     */
+    private final int wateringDuration;
 
     /**
      * Constructor.
@@ -39,22 +58,80 @@ public class MonitoringOrchestrator {
     public MonitoringOrchestrator(SensorList sensorList, Properties settings) {
         this.sensorList = sensorList;
         this.settings = settings;
+
+        // Read the setting called 'greencore.watering.minHumidityBeforeWatering', convert it to a double and save it into a local variable
+        double tempMinHumidity = 0.4;
+        try {
+            tempMinHumidity = Double.parseDouble(settings.getProperty("greencore.watering.minHumidityBeforeWatering"));
+        } catch (NumberFormatException e) {
+            System.out.println("The value of the setting 'greencore.watering.minHumidityBeforeWatering' is not a valid number. The default value will be used: " + tempMinHumidity);
+        } finally {
+            // Save value
+            minHumidity = tempMinHumidity;
+        }
+
+        int tempWateringTime = 600;
+        try {
+            tempWateringTime = Integer.parseInt(settings.getProperty("greencore.watering.wateringTimeMillisecond"));
+        } catch (NumberFormatException e) {
+            System.out.println("The value of the setting 'greencore.watering.wateringTimeMillisecond' is not a valid number. The default value will be used: " + tempWateringTime);
+        } finally {
+            // Save value
+            wateringDuration = tempWateringTime;
+        }
     }
 
     /**
      * Start the monitoring orchestrator.
      * @param config Monitoring configuration.
      */
-    public void startMonitoring (MonitoringConfig config) {
+    public void startMonitoring (MonitoringConfig config, WateringSystem wateringSystem) {
         // Initialize the thread pool
         threadPool.addAll(initializeThreadPool(config));
+
+        // Initialize cyclic barrier
+        barrier = new CyclicBarrier(threadPool.size(), () -> {
+            System.out.println();
+            System.out.println("[Monitoring] All sensor threads have reached the check barrier. Checking soil sensors...");
+
+            // Local variable
+            final AtomicBoolean isWatering = new AtomicBoolean(false);
+
+            // Read all sensors
+            sensorList.getMoistureSensors().forEach(sensor -> {
+                if (!isWatering.get()) {
+                    // Check if sensor is enabled
+                    if (sensor.isEnabled()) {
+                        // Print sensor data
+                        System.out.println(sensor.getName() + ": " + sensor.getCachedValue());
+
+                        // Check if sensor is dry
+                        if (sensor.getCachedValue() < minHumidity) {
+                            // Sensor is dry, water it
+                            System.out.println("Need to water our plants! Thank you " + sensor.getName() + " for having notified me :)");
+
+                            // Set flag to true to avoid watering multiple times
+                            isWatering.set(true);
+
+                            // Water the plants for a while
+                            wateringSystem.turnOnFor(wateringDuration);
+                        }
+                    } else {
+                        // Print sensor disabled message
+                        System.out.println(sensor.getName() + ": disabled");
+                    }
+                }
+            });
+            // Space
+            System.out.println();
+        });
 
         // Start all threads
         System.out.println("[!] Starting individual sensor monitoring...");
         for (Thread thread : threadPool) {
             thread.start();
         }
-        System.out.println("[!] ALl monitor threads have been notified to start successfully.");
+        System.out.println("[!] All monitor threads have been notified to start successfully.");
     }
 
     /**
@@ -65,21 +142,26 @@ public class MonitoringOrchestrator {
     private List<Thread> initializeThreadPool (MonitoringConfig config) {
         List<Thread> tempPool = new ArrayList<>();
         // Create a thread pool
-        int counter = 0;
         for (TemperatureSensor sensor : sensorList.getTemperatureSensors()) {
-            tempPool.add(new Thread(new SensorObserver<>(config, sensor, "TemperatureSensor-" + counter++, settings)));
+            tempPool.add(new Thread(new SensorObserver<>(config, sensor, settings)));
         }
-        counter = 0;
         for (HumiditySensor sensor : sensorList.getHumiditySensors()) {
-            tempPool.add(new Thread(new SensorObserver<>(config, sensor, "HumiditySensor-" + counter++, settings)));
+            tempPool.add(new Thread(new SensorObserver<>(config, sensor, settings)));
         }
-        counter = 0;
         for (MoistureSensor sensor : sensorList.getMoistureSensors()) {
-            tempPool.add(new Thread(new SensorObserver<>(config, sensor, "MoistureSensor-" + counter++, settings)));
+            tempPool.add(new Thread(new SensorObserver<>(config, sensor, settings)));
         }
 
         // Return the thread pool
         return tempPool;
+    }
+
+    /**
+     * Acquire the barrier used to synchronize all threads.
+     * @return A Cyclic barrier.
+     */
+    public static CyclicBarrier acquireBarrier () {
+        return barrier;
     }
 
     /**
