febe1de68b7c06c6c25bc858ce501f0a6fa175c9
luca.dibello@student.supsi.ch <info@lucadibello.ch>
2022-05-17 13:55:23 +0200

    Wait for orchestrator cyclic barrier + sensor enabled/disabled detection

--- greenhouse-iot/GreenCore/backend/src/main/java/com/inhousegreenhouse/ch/backend/orchestrator/SensorObserver.java
+++ greenhouse-iot/GreenCore/backend/src/main/java/com/inhousegreenhouse/ch/backend/orchestrator/SensorObserver.java
@@ -3,15 +3,17 @@ package com.inhousegreenhouse.ch.backend.orchestrator;
 import com.inhousegreenhouse.ch.backend.exception.ProxyRequestFailException;
 import com.inhousegreenhouse.ch.backend.exception.SpiCannotBeInitializedException;
 import com.inhousegreenhouse.ch.backend.model.api.SensorApi;
-import com.inhousegreenhouse.ch.backend.model.sensor.core.ISensor;
+import com.inhousegreenhouse.ch.backend.model.greenhouse.sensor.core.ISensor;
 
 import java.util.Properties;
+import java.util.concurrent.BrokenBarrierException;
 
 /**
  * The sensor observer is the interface that is used to observe the sensor.
+ *
  * @param <T> The type of the sensor that will be observed.
  */
-public class SensorObserver<T extends ISensor<?>> implements Runnable {
+public class SensorObserver<T extends ISensor<? extends Number>> implements Runnable {
     /**
      * The monitoring configuration.
      */
@@ -23,25 +25,19 @@ public class SensorObserver<T extends ISensor<?>> implements Runnable {
     private final T sensor;
 
     /**
-     * The observer thread identifier.
-     */
-    private final String threadIdentifier;
-
-    /**
      * GreenCore proxy URL
      */
     private final String proxyURL;
 
     /**
      * Constructor.
-     * @param config The monitoring configuration.
-     * @param sensor The sensor to observe.
-     * @param threadIdentifier The thread identifier.
+     *
+     * @param config           The monitoring configuration.
+     * @param sensor           The sensor to observe.
      */
-    public SensorObserver(MonitoringConfig config, T sensor, String threadIdentifier, Properties greenhouseSettings) {
+    public SensorObserver(MonitoringConfig config, T sensor, Properties greenhouseSettings) {
         this.config = config;
         this.sensor = sensor;
-        this.threadIdentifier = threadIdentifier;
 
         // Build proxy URL using host and port
         this.proxyURL = "http://" + greenhouseSettings.getProperty("greencore.proxy.host") + ":" + greenhouseSettings.getProperty("greencore.proxy.port") + greenhouseSettings.getProperty("greencore.proxy.route");
@@ -52,7 +48,7 @@ public class SensorObserver<T extends ISensor<?>> implements Runnable {
      */
     @Override
     public void run() {
-        System.out.println("[Thread Orchestrator] " + threadIdentifier + " [CH. " + sensor.getChannel() +": Started sensor observer for sensor " + sensor.getName());
+        System.out.println("[Thread Orchestrator] " + sensor.getName() + " [CH. " + sensor.getChannel() + "]: Started sensor observer for sensor " + sensor.getName());
 
         while (MonitoringOrchestrator.isRunning) {
             // Wait a certain amount of time
@@ -68,25 +64,41 @@ public class SensorObserver<T extends ISensor<?>> implements Runnable {
 
                 // Record data
                 recordData();
+
+                // Update sensor enabled status if disabled
+                if (!sensor.isEnabled()) {
+                    sensor.setEnabled(true);
+                }
             } catch (ProxyRequestFailException e) {
-                System.out.println("[Thread Orchestrator] " + threadIdentifier + ": Error while recording data inside DB. ERROR=" + e.getResponse().errorMessage);
+                System.out.println("[Thread Orchestrator] " + sensor.getName() + ": Error while recording data inside DB. ERROR=" + e.getResponse().errorMessage);
+                if (sensor.isEnabled()) {
+                    sensor.setEnabled(false);
+                }
             } catch (SpiCannotBeInitializedException e) {
                 e.printStackTrace();
+            } finally {
+                // Wait until cyclic barrier is exceeded
+                try {
+                    MonitoringOrchestrator.acquireBarrier().await();
+                } catch (InterruptedException | BrokenBarrierException ex) {
+                    System.out.println("[Thread Orchestrator] " + sensor.getName() + ": Error while waiting for barrier. ERROR=" + ex.getMessage());
+                }
             }
         }
     }
 
     /**
      * Record data inside the database
-     * @throws ProxyRequestFailException If the request to the proxy fails.
+     *
+     * @throws ProxyRequestFailException       If the request to the proxy fails.
      * @throws SpiCannotBeInitializedException If the SPI cannot be initialized.
      */
-    private void recordData () throws ProxyRequestFailException, SpiCannotBeInitializedException {
+    private void recordData() throws ProxyRequestFailException, SpiCannotBeInitializedException {
         // Create sensor api
         SensorApi sensorApi = new SensorApi(proxyURL);
 
         // Record data
         sensorApi.recordData(sensor, config.getGreenhouse());
-        System.out.println("[Thread Orchestrator] " + threadIdentifier + ": Recorded data " + sensor.getCachedValue());
+        System.out.println("[Thread Orchestrator] " + sensor.getName() + ": Recorded data " + sensor.getCachedValue());
     }
 }
